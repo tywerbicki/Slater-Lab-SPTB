c741f554d613970b75f2caeab932c71bfa10333b
Ty Werbicki <86994333+tywerbicki@users.noreply.github.com>
2022-01-06 14:59:14 -0700

    Added file.

---
+++ get_geo.R
@@ -0,0 +1,39 @@
+# Retrieve GEO package on ARC.
+
+BiocManager::install("GEOquery")
+library(GEOquery)
+
+# For Heng et al. data.
+geo_data = getGEO( 
+  GEO = "GSE59491",
+  GSEMatrix = TRUE
+  )
+# For Detroit Data
+geo_data = getGEO( 
+  GEO = "GSE149440",
+  GSEMatrix = TRUE
+)
+
+geo_data = geo_data[[1]]
+
+# Extract assay data.
+write.exprs(geo_data, "exprs_r.txt")
+# Extract pheno data.
+write.csv(pData(geo_data), "pheno_data_r.csv")
+# Extract feature data.
+write.csv(fData(geo_data), "feature_data_r.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
